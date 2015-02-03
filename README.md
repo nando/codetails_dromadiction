c1f794a2ed5baebfb6f2ddd2a03bc5e4714ad851
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2015-02-03 20:16:47 +0100

    First (& maybe also last;) commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # codetails_dromadiction
 Codetail's slides for Terminal Keynote (github.com/fxn/tkn)
+
+    $ git clone https://github.com/nando/codetails_dromadiction
+    $ cd codetails_dromadiction
+    $ ./tkn-src/bin/tkn dromadiction.rb
---
+++ dromadiction.rb
@@ -0,0 +1,152 @@
+# encoding: utf-8
+
+center <<-EOS
+  \e[1mDromadiction\e[0m
+  (AMOR x lo diminuto, parte n+1)
+
+  nando @colgado
+  ©TCK Codetails 2015
+EOS
+
+section "Previously on \e[1m\"AMOR x lo diminuto\"\e[0m..." do
+
+  center <<-EOS
+    En Oviedo corte cuando llegué a
+
+    la descentralización de Twitter
+  EOS
+
+  center <<-EOS
+    Aquí terminé mi charla y os conté
+
+    que su moraleja era semántica
+
+    (y que se la dedicaba a
+     mi primer tuit, Marco [0])
+  EOS
+
+  center <<-EOS
+    Luego vino mi Codetail de
+
+    \e[1mEl Piódromo\e[0m
+
+    (que se lo dediqué a Olivia [1])
+  EOS
+
+  center <<-EOS
+    Y a partir de ahí...
+
+    se me empezó a ir la cabeza
+  EOS
+
+  center <<-EOS
+    Isabel me animaba, pero...
+  EOS
+  center <<-EOS
+    la realidad me abrumaba.
+
+    ¡no podía seguir así!
+  EOS
+  block <<-EOS
+    Me junto con Knapp y Lupión.
+
+    Conclusión:
+     => FOCO para ir cogiendo ritmo
+  EOS
+  center <<-EOS
+    \e[1mEstación M.I.R.\e[0m
+
+    (+ dosis de \e[1mAnder\e[0m a conveniencia)
+  EOS
+
+  center <<-EOS
+    Me cuesta pero puedo...
+
+    ¡y cada día mejor!
+  EOS
+  
+  center <<-EOS
+    Pero hay algo que
+    me lo pone
+    mucho más fácil...
+  EOS
+
+  center <<-EOS
+    \e[1mLA MOTIVACIÓN\e[0m
+  EOS
+
+  center <<-EOS
+    Me doy cuenta de
+    que tengo un problema,
+    ¡y de que es inevitable!
+  EOS
+
+  center <<-EOS
+    Mi problema son...
+
+    \e[1mLAS NOCHES\e[0m
+  EOS
+ 
+  center <<-EOS
+    Que una y otra vez
+    llevan mi cabeza
+    al Piódromo
+  EOS
+
+
+  center <<-EOS
+    Aparco La Estación M.I.R.
+    y comienzo a pedalear de
+    nuevo en El Piódromo
+  EOS
+
+  center <<-EOS
+    \e[1m2 motivaciones 2\e[0m
+
+    * entrenamiento mental *
+    (para programar)
+
+    * herramienta ortopédica*
+    (para recordar)
+  EOS
+
+  center <<-EOS
+    \e[1mmuchas malas ideas\e[0m
+    (estoy seguro)
+    
+    \e[1malguna buena idea\e[0m
+    (es mi ilusión)
+  EOS
+
+  block <<-EOS
+    € export PATH="./bin:$PATH"
+    € git clone git@github.com:nando/auidrome.git
+    € cd auidrome
+    € git submodule update --init
+    € ./bin/symboliclones.sh
+    € ./bin/updatefromcommitted.sh
+    € ./bin/screenthemall.sh -S
+    €  # => and launch ./bin/auidrome.sh on every screen
+    €  # \e[1m¡NO AL TTIP!\e[0m
+  EOS
+
+  center <<-EOS
+    Y periódicamente:
+
+    ./bin/rakepublictuits.sh
+
+    para recuperar lo subido
+    pendiente de comitear
+  EOS
+  
+  center <<-EOS
+    \e[1mto be continued\e[0m
+    (by all of us! :)
+
+    \e[1m¡GRACIAS!\e[0m
+
+    [0] http://otaony.com:3003/tuits/MARCO
+    [1] http://otaony.com:3003/tuits/OLIVIA
+  EOS
+
+end
